39e4929533fc604a5a4a404db1206b4eb2e742c3
김경빈 <154494689+kyeongb-bin@users.noreply.github.com>
2025-03-04 03:12:10 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    if (s.length === 4 || s.length === 6) {
+        if (s == parseInt(s)) {
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
