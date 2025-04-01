41f810673b680531aa8ea7b4981c1076a5442976
김경빈 <154494689+kyeongb-bin@users.noreply.github.com>
2025-04-01 23:22:09 +0900

    [level 0] Title: 정수 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
@@ -1,11 +1,3 @@
 function solution(num_list, n) {
-    let answer = 0;
-    
-    for (let i = 0; i < num_list.length; i++) {
-        if (num_list[i] === n) {
-            answer = 1;
-        }
-    }
-    
-    return answer;
+    return num_list.includes(n) ? 1 : 0;
 }
